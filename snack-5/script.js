8b6cf4edeed4302b9391140a11760cd824c914fc
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-02 13:00:54 +0100

    snack-5

---
+++ snack-5/script.js
@@ -0,0 +1,17 @@
+/*Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
+un numero, se è dispari inseriscilo nell’array.*/
+
+var container = [];
+var numero;
+
+for( var i = 1; i <= 6; i++ ){
+
+    var numero = parseInt(prompt('Inserisci un numero'));
+    
+    if( numero % 2 == 1){
+        container.push(numero);
+    }
+    
+}
+
+console.log(container);
